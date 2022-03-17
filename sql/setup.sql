1b61c9e72bf1de977bd9cb54c92320ced24da622
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-03-17 12:41:38 -0700

    Write insert bike fxnality

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS dogs;
+DROP TABLE IF EXISTS bikes;
 
 CREATE TABLE dogs (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
+    type TEXT NOT NULL
+);
+
+CREATE TABLE bikes (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    brand TEXT NOT NULL,
     type TEXT NOT NULL
 );
